a7f7874b629f8c4540c5a8d57257e6cc44cd2cba
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-31 20:47:12 -0300

    esboço da listagem sem o provider

--- lib/listBills.dart
+++ lib/listBills.dart
@@ -3,6 +3,39 @@ import 'package:flutter/material.dart';
 class ListBills extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contas a pagar"),
+      ),
+      body: 1 == 1 ? Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Nenhuma conta recebida neste momento...",
+                style: TextStyle(
+                    fontSize: 25
+                ),
+              )
+          ): DataTable(
+            columns: <DataColumn>[
+              getColumn('Tipo'),
+              getColumn('Local'),
+              getColumn('Valor'),
+            ],
+            rows: getRows(),
+          ),
+    );
+  }
+
+  DataColumn getColumn(String label) => DataColumn(
+    label: Text(
+      label,
+      style: TextStyle(fontStyle: FontStyle.italic),
+    ),
+  );
+
+  List<DataRow> getRows() {
+    List<DataRow> rows = List();
+
+    return rows;
   }
 }
