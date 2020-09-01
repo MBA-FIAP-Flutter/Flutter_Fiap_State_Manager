9c94c4b8314ceff769ecf79aa989a253a5acc963
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-31 22:38:06 -0300

    adequação da ListBills para Mobx

--- lib/listBills.dart
+++ lib/listBills.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fiap_state_manager/fcm/firebaseNotifications.dart';
-import 'package:flutter_fiap_state_manager/provider/paymentProvider.dart';
+import 'package:flutter_fiap_state_manager/mobx/paymentController.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
 class ListBills extends StatelessWidget {
+
+  PaymentController paymentController;
+
+  ListBills(){
+    paymentController = PaymentController();
+  }
+
   @override
   Widget build(BuildContext context) {
     new FirebaseNotifications(context).setUpFirebase();
@@ -12,6 +20,31 @@ class ListBills extends StatelessWidget {
       appBar: AppBar(
         title: Text("Contas a pagar"),
       ),
+      body: Observer(
+          builder: (_) {
+            if (paymentController.payments.length == 0) {
+              return Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "Nenhuma conta recebida neste momento...",
+                    style: TextStyle(
+                        fontSize: 25
+                    ),
+                  )
+              );
+            } else {
+              return DataTable(
+                columns: <DataColumn>[
+                  getColumn('Tipo'),
+                  getColumn('Local'),
+                  getColumn('Valor'),
+                ],
+                rows: getRows(paymentController),
+              );
+            }
+          }
+      ),
+      /*
       //o consumer é como um listener eterno no ciclo de vida da Widget, ]
       //que responde com o builder do Consumer a qualquer mudança no
       //ChangeNotifier associado
@@ -34,7 +67,7 @@ class ListBills extends StatelessWidget {
             rows: getRows(controller),
           );
         },
-      ),
+      ),*/
     );
   }
 
@@ -48,7 +81,9 @@ class ListBills extends StatelessWidget {
   List<DataRow> getRows(PaymentController controller) {
     List<DataRow> rows = List();
 
-    controller.items.forEach((element) {
+    /*
+    //com provider
+    controller.payments.forEach((element) {
       rows.add(
           DataRow(
             selected: element.selected,
@@ -63,7 +98,25 @@ class ListBills extends StatelessWidget {
             },
           )
       );
-    });
+    });*/
+
+    //mobx
+    for(var index = 0; index < controller.payments.length; index++) {
+      rows.add(
+          DataRow(
+            selected: controller.payments[index].selected,
+            cells: <DataCell>[
+              DataCell(Text(controller.payments[index].type)),
+              DataCell(Text(controller.payments[index].place)),
+              DataCell(Text("${controller.payments[index].value}")),
+            ],
+            onSelectChanged: (value) {
+              controller.payments[index].selected = value;
+              controller.update(index, controller.payments[index]);
+            },
+          )
+      );
+    }
 
     return rows;
   }
